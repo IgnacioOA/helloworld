53a57ff2f13a0b6386eb5b7e514783855e4647f9
IgnacioOA <37209729+IgnacioOA@users.noreply.github.com>
2025-05-11 19:25:17 +0200

    Update api_test.py
    tabulación

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -35,7 +35,7 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "8", "ERROR SQRT"
         )
         
-     def test_api_divide(self):
+    def test_api_divide(self):
         url = f"{BASE_URL}/calc/divide/10/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
